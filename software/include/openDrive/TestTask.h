b5dbd35d62b911b937c2a8e1c3c699c552d09e4e
starrookie <starrookie@dekanews.info>
2011-06-28 12:49:43 +0200

    Added function wqtest framework

---
+++ software/include/openDrive/TestTask.h
@@ -0,0 +1,11 @@
+
+#ifndef __openDrive_TEST_H 
+#define __openDrive_TEST_H
+
+#define testSTACK_SIZE		configMINIMAL_STACK_SIZE
+
+void vStartTestTask( unsigned portBASE_TYPE uxPriority );
+
+
+#endif /* end __openDrive_TEST_H */
+
